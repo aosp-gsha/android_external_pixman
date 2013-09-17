8feda42074db4786f308efdca56a54e1366398f1
John Reck <jreck@google.com>
2013-09-17 16:30:53 -0700

    Crush crush! Make smaller!
    Change-Id: Ic1f476fe1498bd1f8554c82900c66ca019fcd2ac

--- pixman/Android.mk
+++ pixman/Android.mk
@@ -10,36 +10,7 @@ LOCAL_CFLAGS := -DHAVE_CONFIG_H
 
 # Core files
 LOCAL_SRC_FILES := \
-	pixman.c \
-	pixman-access.c \
-	pixman-access-accessors.c \
-	pixman-bits-image.c \
-	pixman-combine32.c \
-	pixman-combine-float.c \
-	pixman-conical-gradient.c \
-	pixman-filter.c \
-	pixman-x86.c \
-	pixman-mips.c \
-	pixman-arm.c \
-	pixman-ppc.c \
-	pixman-edge.c \
-	pixman-edge-accessors.c \
-	pixman-fast-path.c \
-	pixman-glyph.c \
-	pixman-general.c \
-	pixman-gradient-walker.c \
-	pixman-image.c \
-	pixman-implementation.c \
-	pixman-linear-gradient.c \
-	pixman-matrix.c \
-	pixman-noop.c \
-	pixman-radial-gradient.c \
-	pixman-region16.c \
-	pixman-region32.c \
-	pixman-solid-fill.c \
-	pixman-timer.c \
-	pixman-trap.c \
-	pixman-utils.c
+	pixman-matrix.c
 
 # Android additions
 LOCAL_SRC_FILES += \
@@ -49,9 +20,7 @@ ifeq ($(strip $(TARGET_ARCH)),arm)
 	# Will only be used if runtime detection reports NEON capabilities
 	LOCAL_CFLAGS += -DUSE_ARM_NEON
 	LOCAL_SRC_FILES += \
-		pixman-arm-neon.c \
-		pixman-arm-neon-asm.S \
-		pixman-arm-neon-asm-bilinear.S
+		pixman-android-neon.S
 endif
 
 LOCAL_STATIC_LIBRARIES := cpufeatures
